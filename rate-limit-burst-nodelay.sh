71075540f613ba6db6d515d55119d6456a38ec85
Rafael Silvestri <rafael.silvestri@thomsonreuters.com>
2020-10-22 14:30:56 -0300

    small improvements

--- rate-limit-burst-nodelay.sh
+++ rate-limit-burst-nodelay.sh
@@ -3,7 +3,7 @@
 INTERVAL=$1
 
 while true; do
-    RESPONSE=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8084)
+    RESPONSE=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8085)
     WHEN=$(date +"%T") 
     echo "${WHEN} - ${RESPONSE}"
     sleep ${INTERVAL:-0.5}; 
--- unlimited.sh
+++ unlimited.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+INTERVAL=$1
+
 while true; do
     RESPONSE=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8081)
     WHEN=$(date +"%T") 
     echo "${WHEN} - ${RESPONSE}"
-    sleep 0.5; 
+    sleep ${INTERVAL:-0.5};
 done
